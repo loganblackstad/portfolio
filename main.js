8d5b1035badac9ccfd33939e4822845919dce023
Tyler Campbell <tcampb30@gmail.com>
2018-01-29 13:44:48 -0500

    Added drop-down menu for mobile

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
-$('.menu .item').tab();
+const displayItems = (event) => {
+    event.preventDefault();
+    const menu = document.querySelector('[data-menu]');
+    menu.style.display = 'block';
+    // document.querySelector('[data-project-dropdown').setAttribute("style", "border: 2px solid #96c8da; border-bottom: none; border-radius: 0px;");
+    // menu.lastElementChild.setAttribute("style", "border: 2px solid #96c8da; border-top: none;");
+    // menu.firstElementChild.setAttribute("style", "border: 2px solid #96c8da; border-top: none; border-bottom: none;");
+    // document.querySelector('[data-value="1"]').setAttribute("style", "border: 2px solid #96c8da; border-top: none; border-bottom: none;");
+    // document.querySelector('[data-project-dropdown]').style.border = '1px solid #96c8da';
+    // document.querySelector('[data-menu]').lastChild.style.border = '1px solid #96c8da';
+
+   
+}
+
+
+document.querySelector('[data-project-dropdown]').addEventListener('click', displayItems);
+
+// 
+
