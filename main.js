98dbc99c7611779f7dbc19f8e9759723712823f4
Tyler Campbell <tcampb30@gmail.com>
2018-01-30 11:34:00 -0500

    Added smooth scrolling

--- main.js
+++ main.js
@@ -1,5 +1,19 @@
+// Toggle navbar
 document.querySelector('[data-mobile-nav]').addEventListener('click', (event) => {
     event.preventDefault();
     let dropdownMenu = document.querySelector('[data-mobile-nav]').nextElementSibling;
-    dropdownMenu.getAttribute('style').includes('none') ? dropdownMenu.setAttribute('style', 'display:flex') : dropdownMenu.setAttribute('style', 'display:none');
-})
+    dropdownMenu.getAttribute('style').includes('none') ? dropdownMenu.setAttribute('style', 'display:block') : dropdownMenu.setAttribute('style', 'display:none');
+});
+//Add smooth scrolling
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click((event) => {
+        let targetHash = event.currentTarget.hash;
+        event.preventDefault();
+        $('.dropdown-menu').attr('style', 'display: none;');
+        $('html, body').animate({
+          scrollTop: $(targetHash).offset().top
+        }, 1500);
+  });
