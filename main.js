f81f0dc9d39b0bd78cd718ca337a4f273b1e5817
Logan Blackstad <logan.blackstad@gmail.com>
2020-05-18 14:24:29 -0400

    edited content and layout

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 // Toggle navbar
 document.querySelector('[data-mobile-nav]').addEventListener('click', (event) => {
-    event.preventDefault();
-    let dropdownMenu = document.querySelector('[data-mobile-nav]').nextElementSibling;
-    dropdownMenu.getAttribute('style').includes('none') ? dropdownMenu.setAttribute('style', 'display:block') : dropdownMenu.setAttribute('style', 'display:none');
+  event.preventDefault();
+  let dropdownMenu = document.querySelector('[data-mobile-nav]').nextElementSibling;
+  dropdownMenu.getAttribute('style').includes('none') ? dropdownMenu.setAttribute('style', 'display:block') : dropdownMenu.setAttribute('style', 'display:none');
 });
 //Add smooth scrolling
 $('a[href*="#"]')
@@ -10,12 +10,12 @@ $('a[href*="#"]')
   .not('[href="#"]')
   .not('[href="#0"]')
   .click((event) => {
-        let targetHash = event.currentTarget.hash;
-        event.preventDefault();
-        $('.dropdown-menu').attr('style', 'display: none;');
-        $('html, body').animate({
-          scrollTop: $(targetHash).offset().top
-        }, 1500);
+    let targetHash = event.currentTarget.hash;
+    event.preventDefault();
+    $('.dropdown-menu').attr('style', 'display: none;');
+    $('html, body').animate({
+      scrollTop: $(targetHash).offset().top
+    }, 1500);
   });
 //Button anchor links
 $(document).ready(() => {
